4ea75ee1e73d59d006335dd7dfdd0a069ec8e21c
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-02 22:22:51 -0300

    Layout básico del componente Main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Main</p>
+  render() {
+    return (
+      <div>
+        <div class="row">
+          <div class="col-12 col-lg-4">
+            <div class="form-group">
+              <label>Texto</label>
+              <input type="text" class="form-control" />
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
