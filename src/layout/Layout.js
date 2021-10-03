9edd504ca9e40d0b20fc16b0a4c7827b76c1600a
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-02 22:31:50 -0300

    Creado nexto de parámetros entre componentes Layout y Main

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Main from '../main/Main';
 
 class Layout extends React.Component {
-    render() {
-        return (
-          <div>
-            <Main />
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: 'Text',
+    };
+
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(event) {
+    var text = event.target.value;
+    this.setState({ data: text });
+  }
+
+  render() {
+    return (
+      <div>
+        <Main text={this.state.data} changeText={this.changeText} />
+        <hr />
+        <p>Data: {this.state.data}</p>
+      </div>
+    );
+  }
 }
 
 export default Layout;
--- src/main/Main.js
+++ src/main/Main.js
@@ -8,7 +8,18 @@ class Main extends React.Component {
           <div class="col-12 col-lg-4">
             <div class="form-group">
               <label>Texto</label>
-              <input type="text" class="form-control" />
+              <input
+                type="text"
+                class="form-control"
+                value={this.props.text}
+                onChange={this.props.changeText}
+              />
+            </div>
+          </div>
+          <div class="col-12 col-lg-4">
+            <div class="form-group">
+              <label>Texto resultante</label>
+              <input type="text" class="form-control" disabled value={this.props.text} />
             </div>
           </div>
         </div>
